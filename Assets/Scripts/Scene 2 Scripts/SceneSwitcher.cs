712f061e21d32786260a78ecd039b0e1e2f6bca6
Tori <vbialorucki@mail.sfsu.edu>
2022-02-12 02:25:49 -0800

    Button for Ending Game and SceneSwitcher script
    Added end game button for scene 2
    Added SceneSwitcher script to switch between scenes 1 and 2

---
+++ Assets/Scripts/Scene 2 Scripts/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void PlayGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Back(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
