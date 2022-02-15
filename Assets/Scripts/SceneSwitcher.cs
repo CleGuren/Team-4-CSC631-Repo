d733e8ea52c4f22b08b40b2fd0c2b62a3f855fd9
Tori <vbialorucki@mail.sfsu.edu>
2022-02-14 20:40:53 -0800

    changed button highlight

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -10,4 +10,9 @@ public class SceneSwitcher : MonoBehaviour
     public void Back(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
+
+    public void Quit(){
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
 }
