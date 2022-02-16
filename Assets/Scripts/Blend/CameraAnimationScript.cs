90c8efbf726904f9666ca398391af809c8251569
CleGuren <conglewin@gmail.com>
2022-02-15 21:53:59 -0800

    Installed CinnemaMachine and InputSystem packages, working on camera transitioning

---
+++ Assets/Scripts/Blend/CameraAnimationScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraAnimationScript : MonoBehaviour
+{
+    
+    [SerializeField]
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
