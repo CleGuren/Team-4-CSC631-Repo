9ed7a474c23b7556accece1d343c1b647f5509f4
CleGuren <conglewin@gmail.com>
2022-02-18 13:23:18 -0800

    updated camera position when following object, it now remain at a proper distance from the object, this method was done using a script

---
+++ Assets/Scripts/Sub-Group MnC Script/CameraFollowsPlayer.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CameraFollowsPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    // Update is called once per frame
+    void Update() {
+        transform.position = player.position + offset;
+    }
+}
