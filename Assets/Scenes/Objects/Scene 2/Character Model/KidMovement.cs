5f14184bddf805216e2a5dfbd6c164df6b6fa4ed
Tori <vbialorucki@mail.sfsu.edu>
2022-02-23 20:18:21 -0800

    Got Input System Working with Sub-team scenes

--- Assets/Scenes/Objects/Scene 2/Character Model/KidMovement.cs
+++ Assets/Scenes/Objects/Scene 2/Character Model/KidMovement.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class KidMovement : MonoBehaviour
 {
@@ -11,7 +10,7 @@ public class KidMovement : MonoBehaviour
     private string groundTag = "Ground";
     private Animator animator;
     private Rigidbody kid;
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,17 +18,19 @@ public class KidMovement : MonoBehaviour
         animator = GetComponent<Animator>();
         kid = GetComponent<Rigidbody>();
     }
+    
+    
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(1)){
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        animator.SetBool("isMoving", true);
-
-        if(Physics.Raycast(ray, out hit, Mathf.Infinity)){
-            if(hit.collider.CompareTag(groundTag)){
-                agent.SetDestination(hit.point);
+        if(Mouse.current.rightButton.isPressed){
+            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            animator.SetBool("isMoving", true);
+            
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity)){
+                if(hit.collider.CompareTag(groundTag)){
+                    agent.SetDestination(hit.point);
                 }
             }
         }
