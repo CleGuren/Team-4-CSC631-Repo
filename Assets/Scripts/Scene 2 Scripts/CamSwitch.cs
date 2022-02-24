051bff2dd3b3676a247373444a25aa50d95fa4ad
Tori <vbialorucki@mail.sfsu.edu>
2022-02-23 20:18:21 -0800

    Got Input System Working with Sub-team scenes

--- Assets/Scripts/Scene 2 Scripts/CamSwitch.cs
+++ Assets/Scripts/Scene 2 Scripts/CamSwitch.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CamSwitch : MonoBehaviour
 {
@@ -18,7 +19,7 @@ public class CamSwitch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("c")) {
+        if(Keyboard.current.cKey.wasPressedThisFrame) {
             tdCameraEnabled = !tdCameraEnabled;
             mainCamera.enabled = !tdCameraEnabled;
             topDownCamera.enabled = tdCameraEnabled;
